cf64d0f59e4cc2fc362a06fa8eb54b9ae7ab7a46
Stephen Tjasink <stephen@tjasink.com>
2021-12-01 07:22:19 +0000

    Day 1 refactor again - neater but slowed it down

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -15,26 +15,15 @@ class Day01 : Solver {
     }
 
     override fun part2(input: List<String>): Long {
-        val groupedDepths = mutableListOf<Long>()
-        for (i in 0 .. input.size - 3) {
-            groupedDepths.add(input.subList(i, i + 3).sumOf { it.toLong() })
-        }
+        val groupedDepths = input.windowed(3).map { it.sumOf { it.toLong() } }
         return countIncreasingDepths(groupedDepths)
     }
 
     private fun countIncreasingDepths(depths: List<Long>): Long {
-        var totalIncreases: Long = 0
-        var previousDepth: Long? = null
-
-        depths.forEach { depth ->
-            if (previousDepth != null) {
-                if (depth > previousDepth!!) {
-                    totalIncreases += 1
-                }
-            }
-            previousDepth = depth
-        }
-
-        return totalIncreases
+        return depths
+            .windowed(2)
+            .map { depthPair -> depthPair[1] > depthPair[0] }
+            .count { it }
+            .toLong()
     }
 }
