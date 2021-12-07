8d0e1ff5c0e23bd666fa531ff1d5d6a6bc487d3e
Stephen Tjasink <stephen@tjasink.com>
2021-12-07 05:32:05 +0000

    Day 7 refactor

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -12,40 +12,34 @@ fun main() {
 class Day07 : Solver {
 
     override fun part1(input: List<String>): Long {
-        val startPositions = input.first().split(",").map { it.toInt() }
-        val maxPos = startPositions.maxOrNull()!!
-
-
-        val fuelPerPos = Array(maxPos + 1) { 1000 }
-        for (pos in 0 .. maxPos) {
-            val fuelPerSub = startPositions.map { (it - pos).absoluteValue }
-            val totalFuel = fuelPerSub.sum()
-            fuelPerPos[pos] = totalFuel
-        }
-
-        val minFuel = fuelPerPos.minOrNull()!!
-
-        return minFuel.toLong()
+        return calculateMinFuelNeeded(parseStartPositions(input)) { numSpaces -> linearFuelUsage(numSpaces) }.toLong()
     }
 
     override fun part2(input: List<String>): Long {
+        return calculateMinFuelNeeded(parseStartPositions(input)) { numSpaces -> risingFuelUsage(numSpaces) }.toLong()
+    }
+
+    private fun parseStartPositions(input: List<String>): List<Int> {
         val startPositions = input.first().split(",").map { it.toInt() }
+        return startPositions
+    }
+
+    private fun calculateMinFuelNeeded(startPositions: List<Int>, fuelForMove: (Int) -> Int): Int {
         val maxPos = startPositions.maxOrNull()!!
-
-
-        val fuelPerPos = Array(maxPos + 1) { 1000 }
+        val fuelPerPos = Array(maxPos + 1) { Int.MAX_VALUE }
         for (pos in 0 .. maxPos) {
             val fuelPerSub = startPositions.map { fuelForMove((it - pos).absoluteValue) }
             val totalFuel = fuelPerSub.sum()
             fuelPerPos[pos] = totalFuel
         }
-
-        val minFuel = fuelPerPos.minOrNull()!!
-
-        return minFuel.toLong()
+        return fuelPerPos.minOrNull()!!
     }
 
-    private fun fuelForMove(n: Int): Int {
-        return (n * (n + 1)) / 2
+    private fun linearFuelUsage(spaces: Int): Int {
+        return spaces
+    }
+
+    private fun risingFuelUsage(spaces: Int): Int {
+        return (spaces * (spaces + 1)) / 2
     }
 }
