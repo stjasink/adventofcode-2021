5f8e9696d37af367ca884a5e89e130890867183d
Stephen Tjasink <stephen@tjasink.com>
2021-12-14 08:15:57 +0000

    Day 14 part 2

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -11,36 +11,52 @@ fun main() {
 class Day14 : Solver {
 
     override fun part1(input: List<String>): Long {
-        val startString = input.first()
-        val replacements = input.drop(2).map { line ->
-            val (match, newLetter) = line.split(" -> ")
-            val replacement = match[0] + newLetter + match[1]
-            match to replacement
-        }.toMap()
-
-        var polymer = startString
-        for (i in 0 until 10) {
-            val newPolymer = StringBuilder()
-            newPolymer.append(polymer.first())
-            polymer.windowed(2).forEach { pair ->
-                val foundReplacement = replacements[pair]
-                if (foundReplacement == null) {
-                    newPolymer.append(pair)
-                } else {
-                    newPolymer.append(foundReplacement.drop(1))
-                }
-            }
-            polymer = newPolymer.toString()
-        }
-
-        val counts = polymer.toList().groupBy { it }.map { it.key to it.value.size }
-        val minCount = counts.minByOrNull { it.second }!!.second
-        val maxCount = counts.maxByOrNull { it.second }!!.second
-
-        return maxCount.toLong() - minCount.toLong()
+        return calculate(input, 10)
     }
 
     override fun part2(input: List<String>): Long {
-        return 0L
+        return calculate(input, 40)
+    }
+
+    private fun calculate(input: List<String>, iterations: Int): Long {
+        val startString = input.first()
+        val replacementMap = input.drop(2).map { line ->
+            val (match, newLetter) = line.split(" -> ")
+            val replacements = listOf(match[0] + newLetter, newLetter + match[1])
+            match to replacements
+        }.toMap()
+
+        var pairCount = startString.windowed(2).groupBy { it }.map { it.key to it.value.size.toLong() }.toMap()
+        for (i in 0 until iterations) {
+            val newPairCount = mutableMapOf<String, Long>()
+            pairCount.forEach { (pair, count) ->
+                val replacements = replacementMap[pair]
+                replacements!!.forEach { newPair ->
+                    if (newPairCount[newPair] == null) {
+                        newPairCount[newPair] = count
+                    } else {
+                        newPairCount[newPair] = (newPairCount[newPair]!! + count)
+                    }
+                }
+            }
+            pairCount = newPairCount
+        }
+
+        val letterCounts = mutableMapOf<Char, Long>()
+        pairCount.forEach { (pair, count) ->
+            val letter = pair.first()
+            if (letterCounts[letter] == null) {
+                letterCounts[letter] = count
+            } else {
+                letterCounts[letter] = letterCounts[letter]!! + count
+            }
+        }
+        val finalLetter = startString.last()
+        letterCounts[finalLetter] = letterCounts[finalLetter]!! + 1
+
+        val minCount = letterCounts.minByOrNull { it.value }!!.value
+        val maxCount = letterCounts.maxByOrNull { it.value }!!.value
+
+        return maxCount - minCount
     }
 }
