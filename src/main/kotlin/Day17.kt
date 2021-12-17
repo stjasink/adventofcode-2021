736497733b6a0a2338b14ef3b9f3564250217b74
Stephen Tjasink <stephen@tjasink.com>
2021-12-17 07:03:44 +0000

    Day 17 part 2

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -14,13 +14,9 @@ class Day17 : Solver {
     override fun part1(input: List<String>): Long {
         val target = parseTarget(input.first())
 
-//        println("x=$targetXRange, y=$targetYRange")
-
-        var totalMaxY = 0
-
-        // we want to find highest, so always increasing y
-        for (yVel in 1..5000) {
-            for (xVel in 1..target.xRange.last) {
+       var totalMaxY = 0
+        for (yVel in -1000..1000) {
+            for (xVel in -1000..1000) {
                 val (foundTarget, maxY) = plotLaunch(xVel, yVel, target)
                 if (foundTarget) {
                     totalMaxY = max(totalMaxY, maxY)
@@ -32,7 +28,19 @@ class Day17 : Solver {
     }
 
     override fun part2(input: List<String>): Long {
-        return 0L
+        val target = parseTarget(input.first())
+
+        val landed = mutableSetOf<Pair<Int, Int>>()
+        for (yVel in -1000..1000) {
+            for (xVel in -1000..1000) {
+                val (foundTarget, _) = plotLaunch(xVel, yVel, target)
+                if (foundTarget) {
+                    landed.add(Pair(xVel, yVel))
+                }
+            }
+        }
+
+        return landed.size.toLong()
     }
 
     fun plotLaunch(xVelStart: Int, yVelStart: Int, target: Target): Pair<Boolean, Int> {
@@ -49,7 +57,7 @@ class Day17 : Solver {
             y += yVel
             yMax = max(yMax, y)
 
-            xVel = if (xVel > 0) xVel - 1 else xVel + 1
+            xVel = if (xVel > 0) xVel - 1 else if (xVel < 0) xVel + 1 else 0
             yVel -= 1
 
         } while (!isPastTarget(x, y, target))
--- src/test/kotlin/Day17Test.kt
+++ src/test/kotlin/Day17Test.kt
@@ -23,6 +23,41 @@ class Day17Test {
     }
 
     @Test
+    fun part2TestPlot1() {
+        assertTrue(Day17().plotLaunch(7, 6, Day17.Target(20..30, -10..-5)).first)
+    }
+
+    @Test
+    fun part2TestPlot2() {
+        assertTrue(Day17().plotLaunch(7, 7, Day17.Target(20..30, -10..-5)).first)
+    }
+
+    @Test
+    fun part2TestPlot3() {
+        assertTrue(Day17().plotLaunch(7, 8, Day17.Target(20..30, -10..-5)).first)
+    }
+
+    @Test
+    fun part2TestPlot4() {
+        assertTrue(Day17().plotLaunch(7, 9, Day17.Target(20..30, -10..-5)).first)
+    }
+
+    @Test
+    fun part2TestPlot5() {
+        assertFalse(Day17().plotLaunch(5, 7, Day17.Target(20..30, -10..-5)).first)
+    }
+
+    @Test
+    fun part2TestPlot6() {
+        assertFalse(Day17().plotLaunch(5, 8, Day17.Target(20..30, -10..-5)).first)
+    }
+
+    @Test
+    fun part2TestPlot7() {
+        assertFalse(Day17().plotLaunch(5, 9, Day17.Target(20..30, -10..-5)).first)
+    }
+
+    @Test
     fun part1Test() {
         val input = """
             target area: x=20..30, y=-10..-5
@@ -39,6 +74,6 @@ class Day17Test {
         """.trimIndent().split('\n')
 
         val answer = Day17().part2(input)
-        assertEquals(0L, answer)
+        assertEquals(112L, answer)
     }
 }
